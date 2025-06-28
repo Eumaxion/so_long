ca86b21b1b026a4fda7191f89a0c168b662e1165
Eumaxion <marciorock171@gmail.com>
2025-06-28 01:47:14 +0100

    small change

--- render_map.c
+++ render_map.c
@@ -5,15 +5,15 @@ void	put_image(t_game *data, int x, int y)
 	char	character;
 	character = data->tmap.map[y][x];
 	if (character == '0')
-		mlx_put_image_to_window(data->mlx, data->win, data->tmap.img_backg, x, y);
+		mlx_put_image_to_window(data->mlx, data->win, data->tmap.img_backg, x * PIXS, y * PIXS);
  	else if (character == 'P')
-		mlx_put_image_to_window(data->mlx, data->win, data->t_player.player_right, x , y);
+		mlx_put_image_to_window(data->mlx, data->win, data->t_player.player_right, x * PIXS, y * PIXS);
 	else if (character == 'C')
-		mlx_put_image_to_window(data->mlx, data->win, data->tmap.img_colect, x , y);
+		mlx_put_image_to_window(data->mlx, data->win, data->tmap.img_colect, x * PIXS, y * PIXS);
 	else if (character == 'E')
-		mlx_put_image_to_window(data->mlx, data->win, data->tmap.img_exit, x, y);
+		mlx_put_image_to_window(data->mlx, data->win, data->tmap.img_exit, x * PIXS, y * PIXS);
 	else if (character == '1')
-		mlx_put_image_to_window(data->mlx, data->win, data->tmap.img_wall, x, y);
+		mlx_put_image_to_window(data->mlx, data->win, data->tmap.img_wall, x * PIXS, y * PIXS);
 }
 
 void render_map(t_game *data)
